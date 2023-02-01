ee5d874d3a4fb02c8f745381e71344abc16f8811
habeneyasu2004 <haben.eyasu2004@gmail.com>
2023-01-31 23:01:06 -0500

    Finding the Kth Largest Integer in the Array

---
+++ leetcode/Kth-Largest-Integer-1985.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+static bool compare(string &s1,string &s2)
+    {
+        if(s1.size()==s2.size())
+        {
+            return s1<s2;
+        }
+        return s1.size()<s2.size();
+    }
+    string kthLargestNumber(vector<string>& nums, int k) {
+      sort(nums.begin(),nums.end(),compare);
+        return nums[nums.size()-k];
+    }
+};
